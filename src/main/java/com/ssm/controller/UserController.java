06387441ed57a819557f0bf3b210057ce21c166f
Sea for Mac <18912387311@163.com>
2019-08-29 22:37:09 +0800

    路由

--- src/main/java/com/ssm/controller/UserController.java
+++ src/main/java/com/ssm/controller/UserController.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
 
-    @RequestMapping("hello")
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
     public ModelAndView hello(){
         ModelAndView mav=new ModelAndView();
         mav.addObject("msg","Hello Spring MVC!");
@@ -18,14 +20,14 @@ public class UserController {
         return mav;
     }
 
-    @RequestMapping("hello2")
+    @RequestMapping(value = "hello2",method = RequestMethod.GET)
     public String hello2(Model model,Integer id){
         model.addAttribute("msg","Hello Spring MVC!");
         System.out.println(id);
         return "Test";
     }
 
-    @RequestMapping("save")
+    @RequestMapping(value = "save",method = RequestMethod.POST)
     public String save(Model model, User user){
         model.addAttribute("msg","保存成功!");
         return "Test";
