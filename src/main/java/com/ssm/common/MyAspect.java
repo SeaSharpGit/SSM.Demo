c386a388accffc0feaa3269124f4830708383f4c
Sea for Mac <18912387311@163.com>
2020-03-24 19:30:24 +0800

    aspect

--- src/main/java/com/ssm/common/MyAspect.java
+++ src/main/java/com/ssm/common/MyAspect.java
@@ -1,4 +1,4 @@
-package com.ssm.aspect;
+package com.ssm.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -11,22 +11,22 @@ public class MyAspect {
     @Pointcut(value = "execution(* com.ssm.service.UserService.sayHello(..))")
     private void sayHelloPointCut(){}
 
-    @Before(value = "MyAspect.sayHelloPointCut()")
+    @Before(value = "com.ssm.common.MyAspect.sayHelloPointCut()")
     public void before(){
         System.out.println("MyAspect.before");
     }
 
-    @After(value = "MyAspect.sayHelloPointCut()")
+    @After(value = "com.ssm.common.MyAspect.sayHelloPointCut()")
     public void after(){
         System.out.println("MyAspect.after");
     }
 
-    @AfterReturning(value = "MyAspect.sayHelloPointCut()",returning = "result")
+    @AfterReturning(value = "com.ssm.common.MyAspect.sayHelloPointCut()",returning = "result")
     public void afterReturning(Object result){
         System.out.println("MyAspect.afterReturning:"+result.toString());
     }
 
-    @Around(value = "MyAspect.sayHelloPointCut()")
+    @Around(value = "com.ssm.common.MyAspect.sayHelloPointCut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("MyAspect环绕前");
         Object obj=joinPoint.proceed();
@@ -34,7 +34,7 @@ public class MyAspect {
         return obj;
     }
 
-    @AfterThrowing(value = "MyAspect.sayHelloPointCut()",throwing = "ex")
+    @AfterThrowing(value = "com.ssm.common.MyAspect.sayHelloPointCut()",throwing = "ex")
     public void afterThrowing(Throwable ex){
         System.out.println("MyAspect.afterThrowing："+ex.getMessage());
     }
